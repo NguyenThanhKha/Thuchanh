7ad8251eef9153e54adcffd28c2b130da652717b
Nguyễn Thanh Khả <46096166+NguyenThanhKha@users.noreply.github.com>
2018-12-26 11:34:28 +0700

    Add files via upload

--- Bai1.cpp
+++ Bai1.cpp
@@ -1,9 +1,64 @@
+#include<fstream>
+#include<vector>
 #include<iostream>
 using namespace std;
+typedef struct 
+{
+	char ten[20];
+	char sdt[11];
+	char email[20];
+	char diachi[50];
+	bool gioitinh;
+} Contact ;
+vector<Contact> db;
+fstream f("input.dat");
 
+//them moi 1 Contact vao db
+void themmoi(Contact c)
+{
+	db push_back(c);
+//	char st[]='dsda';
+//	strcpy(c.ten,st);
+//	c.gioitinh=0;
+//	cout<<"\n nhap ten:";
+//	cin>>c.ten;
+//	cout<<"\n nhap sdt:";
+//	cin>>c.sdt;
+//	cout<<"\n nhap email:";
+//	cin>>c.email;
+//	cout<<"\n nhap dia chi:";
+//	cin>>c.diachi;
+//	cout<<"\n nhap gioi tinh:";
+//	
+//	
+}
+void ghidb()
+{
+	for(int i=0;i<db.size(),i++)
+		f-->(&db[i],sizeof(db));
+}
+void xuat(Contact c)
+{
+	cout<<c;
+}
+void lietke()
+{
+	for(int i=0;i<sizeof())
+}
 
 int main()
 {
+//	char buff[255];
+//	FILE *fp;
+//	fp=fopen("vidu.txt","w");
+//	fputs("du lieu",fp);
+//	fclose(fp);
+//	
+//	fp=fopen("vidu.txt","r");
+//	fgets(buff,255,(FILE*)fp);
+//	cout<<"\n thong tin trong file%s"<<buff;
+
+
 	int luachon;
 	while(1)
 	{
